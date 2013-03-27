f7af77b5f5dd1c7d3f4bb76da9329a9053002959
Terry Fan <terry.tr_fan@htc.com>
2013-03-27 10:46:49 +0800

    UPD

--- src/main/java/org/ops/compress/deflate/App.java
+++ src/main/java/org/ops/compress/deflate/App.java
@@ -1,6 +1,10 @@
 package org.ops.compress.deflate;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.zip.Inflater;
@@ -16,42 +20,51 @@ public class App
 {
     public static void main( String[] args )
     {
-        if (args.length > 0) {
-	    try{
-                deflateAndBase64(args[0]);
-	    } catch (Exception e) {
-		System.out.println(e);
-	    }
-	}
+        if (args.length > 0)
+        {
+            try {
+                byte[] output = deflateAndBase64(args[0]);
+                OutputStream out = new FileOutputStream("output.dat");
+                out.write(output, 0, output.length);
+                out.flush();
+                out.close();
+            } catch (Exception e) {
+                System.out.println(e);
+                e.printStackTrace();
+            e
+
+        }
     }
 
-    public static String deflateAndBase64 (String message) throws Exception
+    public static byte[] deflateAndBase64 (String message) throws Exception
     {
         byte[] decodedBytes = Base64.decodeBase64(message);
         if(decodedBytes == null)
-	{
+        {
             throw new UnsupportedEncodingException("Unable to Base64 decode incoming message");
         }
         
         try
-	{
+        {
             ByteArrayInputStream bytesIn = new ByteArrayInputStream(decodedBytes);
             InflaterInputStream inflater = new InflaterInputStream(bytesIn, new Inflater(true));
 
-	    String output = new String();
-
-	    int read = 0;
-	    do {
-	      byte[] buffer = new byte[2048];
-	      read = inflater.read(buffer, 0, buffer.length);
-
-	      output = output + new String(buffer);
-	    }
-	    while (read > 0);
-	     
-	    System.out.println(output);
-	    return output;
-	} catch (Exception e) {
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            int read = 0;
+            do {
+              byte[] buffer = new byte[2048];
+              read = inflater.read(buffer, 0, buffer.length);
+              if (read > 0)
+              {
+                output.write(buffer, 0, read);
+              }
+            }
+            while (read > 0);
+             
+            return output.toByteArray();
+        }
+        catch (Exception e)
+        {
             throw new Exception("Unable to Base64 decode and inflate SAML message", e);
         }
     } 
